f881de821e1dcfa8e83d5849ed508de569e11fda
Liza <lizaapostol378@gmail.com>
2024-12-22 18:54:02 +0200

    add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import styles from "./App.module.css";
 import ContactForm from "./components/ContactForm/ContactForm";
@@ -6,12 +6,18 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
 function App() {
-  const [contact, setContact] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contact, setContact] = useState(() => {
+    const savedData = JSON.parse(localStorage.getItem("phonebook"));
+
+    return savedData.length > 0
+      ? savedData
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  });
 
   const [filter, setFilter] = useState("");
 
@@ -37,6 +43,10 @@ function App() {
   const handleClick = (id) => {
     setContact((prev) => prev.filter((contact) => contact.id !== id));
   };
+
+  useEffect(() => {
+    localStorage.setItem("phonebook", JSON.stringify(contact));
+  }, [contact]);
 
   return (
     <div className={styles.wrapper}>
